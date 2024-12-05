775723413dbc6791ada34322b99c03091ec2a4db
seonhoshin <sunlike0508@gmail.com>
2024-12-05 23:26:50 +0900

    feat : 순수라이브러리

---
+++ src/test/java/memory/MemoryFinderTest.java
@@ -0,0 +1,14 @@
+package memory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+public class MemoryFinderTest {
+
+    @Test
+    void get() {
+        MemoryFinder memoryFinder = new MemoryFinder();
+        Memory memory = memoryFinder.get();
+        assertThat(memory).isNotNull();
+    }
+}
